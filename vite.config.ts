96537ba24e09dbab8ef57fb752ebd93024db4230
Yiero <aluibyiero@qq.com>
2026-08-25 15:18:42 +0800

    chore: 优化构建和脚本描述

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve, dirname } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import { builtinModules } from 'module';
 import { fileURLToPath } from 'url';
@@ -125,33 +125,36 @@ function copyAssetsPlugin() {
     };
 }
 
-export default defineConfig({
-    resolve: {
-        conditions: ['node', 'default'],
-    },
-    build: {
-        sourcemap: false,
-        target: 'esnext',
-        minify: false,
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            formats: ['es'],
-            fileName: () => 'index.mjs',
+export default defineConfig( ( { mode } ) => {
+    const env = loadEnv( mode, __dirname, '' );
+    return {
+        resolve: {
+            conditions: [ 'node', 'default' ],
         },
-        rollupOptions: {
-            external: [...nodeModules, ...external],
-            output: {
-                inlineDynamicImports: true,
+        build: {
+            sourcemap: false,
+            target: 'esnext',
+            minify: false,
+            lib: {
+                entry: resolve( __dirname, 'src/index.ts' ),
+                formats: [ 'es' ],
+                fileName: () => 'index.mjs',
             },
+            rollupOptions: {
+                external: [ ...nodeModules, ...external ],
+                output: {
+                    inlineDynamicImports: true,
+                },
+            },
+            outDir: 'dist',
         },
-        outDir: 'dist',
-    },
-    plugins: [nodeResolve(), copyAssetsPlugin(), napcatHmrPlugin({
-        webui: {
-            distDir: './src/webui/dist',
-            targetDir: 'webui',
-        },
-        wsUrl: 'ws://120.76.230.235:8998',
-        token: 'kw1l5ob7q4duc32jqqwsron4jsmy2inihi2zmns4937oi5jhddplxfg',
-    })],
-});
+        plugins: [ nodeResolve(), copyAssetsPlugin(), napcatHmrPlugin( {
+            webui: {
+                distDir: './src/webui/dist',
+                targetDir: 'webui',
+            },
+            wsUrl: env.WS_URL,
+            token: env.TOKEN,
+        } ) ],
+    };
+} );
